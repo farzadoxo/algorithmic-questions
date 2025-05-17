15750c60128043dc46f4319593350c77caca7fc9
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-05-17 20:59:35 +0330

    - unsolved

---
+++ Leetcode/Two_Sum.py
@@ -0,0 +1,24 @@
+
+# UNSOLVED
+
+# nums_lsit = [
+#     [2,7,11,15],
+#     [3,2,4],
+#     [3,3]
+# ]
+
+# target_list = [9,6,6]
+
+# def two_sum(nums:list , target:int):
+#     x =0
+#     for i in nums:
+#         x +1
+#         for j in nums:
+#             if x>1:
+#                 if i + j == target:
+#                     return f"[{nums.index(i)},{nums.index(j)}]"
+
+# if __name__ == "__main__":
+#     print(two_sum(nums=nums_lsit[0],target=target_list[0]))
+#     print(two_sum(nums=nums_lsit[1],target=target_list[1]))
+#     print(two_sum(nums=nums_lsit[2],target=target_list[2]))
