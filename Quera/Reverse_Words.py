7ebee0d71901c978ec004c43a3789f28b3d32a19
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-06-21 12:08:30 +0330

    reverse words

---
+++ Quera/Reverse_Words.py
@@ -0,0 +1,11 @@
+
+def reverse_word(n:int,words:str):
+    if len(words.split(" ")) != n :
+           return -1
+    else:
+        word_list = words.split(" ")
+        word_list.reverse()
+        return " ".join(word_list)
+
+
+print(reverse_word(n=int(input()),words=input()))
